60fcae8770c1a4e1acc77e150ededf11d57a38e1
Shawn Sherwood <shawn.sherwood@gmail.com>
2023-11-22 11:03:58 -0500

    Added migration readme with more details in migration files

--- src/main/resources/db/migration/V1_0__create_device_tracker_schema.sql
+++ src/main/resources/db/migration/V1_0__create_device_tracker_schema.sql
@@ -1,3 +1,7 @@
+--
+-- Assumes Flyway will create the schema for the target database
+--
+
 CREATE TABLE yb_device_tracker
 (
     device_id    uuid        NOT NULL,
@@ -8,8 +12,11 @@ CREATE TABLE yb_device_tracker
     CONSTRAINT yb_device_tracker_pkey PRIMARY KEY (device_id HASH, media_id ASC)
 ) SPLIT INTO 100 TABLETS;
 
-create index on yb_device_tracker ((yb_hash_code(updated_date) % 16) asc, updated_date desc) split at values( (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12), (13), (14) );
+-- makes use of a composite hash % mod + range index technique for dates
+-- this can run be non-concurrently because the table was just created above
+create index nonconcurrently on yb_device_tracker ((yb_hash_code(updated_date) % 16) asc, updated_date desc) split at values( (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (11), (12), (13), (14) );
 
+-- load in some sample data that can be used for testing
 insert into yb_device_tracker (device_id, media_id, status, updated_date)
 select uuid('cdd7cacd-8e0a-4372-8ceb-' || lpad(seq::text, 12, '0')),
        '48d1c2c2-0d83-43d9-' || lpad(seq2::text, 4, '0') || '-' || lpad(seq::text, 12, '0'),
--- src/main/resources/db/migration/V1_1__create_device_tracker2_schema.sql
+++ src/main/resources/db/migration/V1_1__create_device_tracker2_schema.sql
@@ -1,3 +1,7 @@
+--
+-- Adding second device tracker table variant for testing
+--
+
 CREATE TABLE yb_device_tracker2
 (
     device_id    uuid        NOT NULL,
@@ -9,12 +13,13 @@ CREATE TABLE yb_device_tracker2
     CONSTRAINT yb_device_tracker2_pkey PRIMARY KEY (device_id HASH, media_id ASC)
 ) SPLIT INTO 30 TABLETS;
 
-create index on yb_device_tracker2(account_id) split into 30 tablets;
+create index nonconcurrently on yb_device_tracker2 (account_id) split into 30 tablets;
 
 insert into yb_device_tracker2 (device_id, media_id, account_id, status, updated_date)
 select uuid('cdd7cacd-8e0a-4372-8ceb-' || lpad(seq::text, 12, '0')),
        '48d1c2c2-0d83-43d9-' || lpad(seq2::text, 4, '0') || '-' || lpad(seq::text, 12, '0'),
        uuid('ff710c59-1e6d-47f9-a775-' || lpad(seq::text, 12, '0')),
-       'ACTIVE' || seq, clock_timestamp()
+       'ACTIVE' || seq,
+       clock_timestamp()
 from generate_series(0, 5000) as seq,
      generate_series(0, 6) seq2;
