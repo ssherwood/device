6ac26801000f613569430c14f04fec4185cbacb7
Shawn Sherwood <shawn.sherwood@gmail.com>
2024-02-21 14:20:22 -0500

    Experiment

--- src/main/java/com/example/demo/device/DeviceApplication.java
+++ src/main/java/com/example/demo/device/DeviceApplication.java
@@ -2,10 +2,13 @@ package com.example.demo.device;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+//@EnableRetry(order = Ordered.LOWEST_PRECEDENCE - 1)
 public class DeviceApplication {
 
     public static void main(String[] args) {
